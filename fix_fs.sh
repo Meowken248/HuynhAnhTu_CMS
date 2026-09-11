9f52d86cad84668adf8884de93872c085774e851
ken248 <anh2482006@gmail.com>
2026-09-11 09:02:11 +0700

    cms_buoi_1: fix YouTube embed with correct iframe from YouTube share

--- fix_fs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i "/That's all, stop editing/i define('FS_METHOD', 'direct');" /var/www/html/wp-config.php
-grep 'FS_METHOD' /var/www/html/wp-config.php && echo "Done!" || echo "Failed"
--- fix_youtube_embed.sh
+++ fix_youtube_embed.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-NEW_IFRAME='<figure class="wp-block-embed"><div class="wp-block-embed__wrapper"><iframe width="100%" height="400" src="https://www.youtube.com/embed/ajEDPzS-BAg" title="Sports Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="border-radius:8px;"></iframe></div></figure>'
+IFRAME='<iframe width="100%" height="400" src="https://www.youtube.com/embed/ajEDPzS-BAg?si=V1gOH8tIfFbk7KcJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="border-radius:8px;display:block;"></iframe>'
 
 for POST_ID in 30 32 33; do
+  # Lấy content hiện tại, xóa hết iframe/figure cũ, thêm iframe mới vào cuối
   OLD=$(wp --allow-root post get $POST_ID --field=post_content)
-  # Xóa toàn bộ iframe cũ và thay bằng iframe mới
-  NEW=$(echo "$OLD" | sed 's|<figure class="wp-block-embed">.*</figure>||g')
-  FINAL="$NEW $NEW_IFRAME"
-  wp --allow-root post update $POST_ID --post_content="$FINAL"
-  echo "Updated post $POST_ID with new YouTube video"
+  # Xóa block figure cũ
+  CLEAN=$(echo "$OLD" | sed 's|<figure[^>]*>.*</figure>||g')
+  # Thêm iframe mới vào cuối
+  NEW="$CLEAN $IFRAME"
+  wp --allow-root post update $POST_ID --post_content="$NEW"
+  echo "Updated post $POST_ID"
 done
 
 echo "ALL DONE!"
